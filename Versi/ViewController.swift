416d484654856f819927ff33454afbc0993fd2c7
Noel-96 <noelorion69@gmail.com>
2019-05-24 15:33:01 +0100

    Initial Commit

---
+++ Versi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Versi
+//
+//  Created by Noel Obaseki on 24/05/2019.
+//  Copyright © 2019 Tizeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
