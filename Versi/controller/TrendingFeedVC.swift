fe39a51d37fa4746ae5204c28bd54bf90017bd37
Noel-96 <noelorion69@gmail.com>
2019-05-29 14:45:22 +0100

    sets up Trending GitHub repos table

--- Versi/controller/TrendingFeedVC.swift
+++ Versi/controller/TrendingFeedVC.swift
@@ -13,31 +13,31 @@ import RxCocoa
 class TrendingFeedVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-//
-//    let refreshControl = UIRefreshControl()
-//
-//    var dataSource = PublishSubject<[Repo]>()
-//    var disposeBag = DisposeBag()
+   
+    let refreshControl = UIRefreshControl()
+
+    var dataSource = PublishSubject<[Repo]>()
+    var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tableView.refreshControl = refreshControl
-//        refreshControl.tintColor = #colorLiteral(red: 0.2784313725, green: 0.462745098, blue: 0.9019607843, alpha: 1)
-//        refreshControl.attributedTitle = NSAttributedString(string: "Fetching Hot Github Repos 🔥", attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.2784313725, green: 0.462745098, blue: 0.9019607843, alpha: 1), NSAttributedStringKey.font: UIFont(name: "AvenirNext-DemiBold", size: 16.0)!])
-//        refreshControl.addTarget(self, action: #selector(fetchData), for: .valueChanged)
-//
-//        fetchData()
-//        dataSource.bind(to: tableView.rx.items(cellIdentifier: "trendingRepoCell")) { (row, repo: Repo, cell: TrendingRepoCell) in
-//            cell.configureCell(repo: repo)
-//        }.addDisposableTo(disposeBag)
+        tableView.refreshControl = refreshControl
+        refreshControl.tintColor = #colorLiteral(red: 0.2784313725, green: 0.462745098, blue: 0.9019607843, alpha: 1)
+        refreshControl.attributedTitle = NSAttributedString(string: "Fetching Hot Github Repos 🔥", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.2784313725, green: 0.462745098, blue: 0.9019607843, alpha: 1), NSAttributedString.Key.font: UIFont(name: "AvenirNext-DemiBold", size: 16.0)!])
+        refreshControl.addTarget(self, action: #selector(fetchData), for: .valueChanged)
+
+        fetchData()
+        dataSource.bind(to: tableView.rx.items(cellIdentifier: "trendingRepoCell")) { (row, repo: Repo, cell: TrendingRepoCell) in
+            cell.configureCell(repo: repo)
+       }.addDisposableTo(disposeBag)
     }
     
-//    @objc func fetchData() {
-//        DownloadService.instance.downloadTrendingRepos { (trendingRepoArray) in
-//            self.dataSource.onNext(trendingRepoArray)
-//            self.refreshControl.endRefreshing()
-//        }
-//    }
+    @objc func fetchData() {
+        DownloadService.instance.downloadTrendingRepos { (trendingRepoArray) in
+            self.dataSource.onNext(trendingRepoArray)
+           // self.refreshControl.endRefreshing()
+        }
+    }
     
 }
 
---
+++ Versi/utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  versi-app
+
+//  Created by Noel Obaseki on 24/05/2019.
+//  Copyright © 2019 Tizeti. All rights reserved.
+//
+
+import Foundation
+
+// MARK – URLs
+let trendingRepoUrl = "https://api.github.com/search/repositories?q=Swift&sort=stars&order=desc"
+let searchUrl = "https://api.github.com/search/repositories?q="
+let starsDescendingSegment = "&sort=stars&order=desc"
+let readmeSegment = "/blob/master/README.md"
